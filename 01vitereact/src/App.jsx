e69019c06f7a6d13cbb11c73cee674bb12e70795
suhzeet <sujeetneupane28@gmail.com>
2024-04-30 21:42:58 +0545

    created the files of react and reat with vite and form a minimal site from them

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>hello guys , welcome to react with vite</h1>;
+}
+
+export default App;
---
+++ react-learning/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>hello guys welcome</h1>;
+}
+
+export default App;
