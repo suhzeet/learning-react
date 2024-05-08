3b6a0c4731fb2f5cc18d28a0339a6692d72dfc6b
suhzeet <sujeetneupane28@gmail.com>
2024-05-08 19:57:33 +0545

    first commit

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <Card username="Sujit Neupane" btnText="hello" />
+    </>
+  );
+}
+
+export default App;
